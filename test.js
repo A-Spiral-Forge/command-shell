d176806bda362ee1a65f9ba93bcaa8b1593fc6da
ABHAY PARIHAR <64008676+A-Spiral-Forge@users.noreply.github.com>
2022-12-05 17:26:21 +0530

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-var startTime = new Date;
-
-setTimeout(function () {
-  var endTime = new Date;
-  console.log('Time elapsed: ', endTime - startTime, 'ms');
-}, 500);
-
-while (new Date - startTime < 1000) {};
